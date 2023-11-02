0c63c94202ca7be4e5ff023ddf0b709f41fae04d
Sean <seahnnnnnn@gmail.com>
2023-11-02 16:33:04 +0900

    feat: program 페이지 구축

--- containers/program/index.tsx
+++ containers/program/index.tsx
@@ -1,8 +1,16 @@
-export const ProgramContainer = () => {
+import { ProgramExplanation } from './fragments/ProgramExplanation'
+import { ContentsSelection } from '@/components/ContentsSelection'
+
+export const ProgramContainer: React.FC = () => {
     return (
-        <div>
-            프로그램
-        </div>
+        <>
+            <ProgramExplanation />
+
+            <ContentsSelection 
+                headingList={["Opening Day", "Workshop", "Talk Concert", "Homecoming Day"]}
+                contentsByHeading={["Content1 - Opening Day", "Content2 - Workshop", "Content3 - Talk Concert", "Content4 - Homecoming Day"]}
+            />
+        </>
     )
 }
 
