11022b4bddf672060dcb4ff3dccb6a21234e9193
Sean <seahnnnnnn@gmail.com>
2023-11-13 15:03:25 +0900

    feat: 수정사항 반영

--- components/ContentsSelection.tsx
+++ components/ContentsSelection.tsx
@@ -1,41 +1,26 @@
-import React, { ReactNode, useState } from "react";
+import React, { useState } from "react";
 
 export const ContentsSelection = ({ headingList, contentsByHeading }) => {
-    const [selected, setSelected] = useState(0);
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
     return (
     <>
         <div className="flex flex-row w-[55%] justify-between ml-[22.5%] mt-[52.5px]">
             {headingList.map((heading:string, index:number) => (
-                selected === index ? (
-                    <div key={index} className="flex flex-col items-center cursor-pointer" onClick={() => setSelected(index)}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
-                            <ellipse cx="6.62058" cy="6.59934" rx="6.62058" ry="6.59934" transform="matrix(1 0 0 -1 0.722656 13.9414)" fill="white"/>
-                        </svg>
-                        <p className="text-white whitespace-pre-wrap text-[41.387px] font-normal font-snu-regular text-center mt-[19.8px]">{heading}</p>
-                    </div>
-                ) : (
-                    <div key={index} className="flex flex-col items-center cursor-pointer" onClick={() => setSelected(index)}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
-                            <ellipse cx="6.62058" cy="6.59934" rx="6.62058" ry="6.59934" transform="matrix(1 0 0 -1 0.722656 13.9414)" fill="#5A5A5A"/>
-                        </svg>
-                        <p className="text-[#5A5A5A] whitespace-pre-wrap text-[41.387px] font-normal font-snu-regular text-center mt-[19.8px]">{heading}</p>
-                    </div>
-                )
+                <div key={index} className="flex flex-col items-center cursor-pointer" onClick={() => setSelectedIndex(index)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                        <ellipse cx="6.62058" cy="6.59934" rx="6.62058" ry="6.59934" transform="matrix(1 0 0 -1 0.722656 13.9414)" fill={selectedIndex === index ? "#FFFFFF" : "#5A5A5A"}/>
+                    </svg>
+                    <p className={`${selectedIndex === index ? "text-[#FFFFFF]" : "text-[#5A5A5A]"} whitespace-pre-wrap text-[41.387px] font-normal font-snu-regular text-center mt-[19.8px]`}>{heading}</p>
+                </div>
             ))}
         </div>
 
         <div className="relative">
             {contentsByHeading.map((content:()=>JSX.Element, index:number) => (
-                selected === index ? (
-                    <div key={index} className="opacity-100 transition-opacity duration-500 ease-in-out absolute w-[55%] ml-[22.5%]">
-                        {content()}
-                    </div>
-                ) : (
-                    <div key={index} className="opacity-0 transition-opacity duration-500 ease-in-out absolute w-[55%] ml-[22.5%]">
-                        {content()}
-                    </div>
-                )
+                <div key={index} className={`${selectedIndex === index ? "opacity-100" : "opacity-0"} transition-opacity duration-500 ease-in-out absolute w-[55%] ml-[22.5%]`}>
+                    {content()}
+                </div>
             ))}
         </div>
     </>
--- containers/about/fragments/ThanksTo.tsx
+++ containers/about/fragments/ThanksTo.tsx
@@ -1,42 +1,33 @@
+const team1 = "지도교수님";
+const teamMates1 = "김수정 배민기 이장섭 이준원 안성모 유병준 장성연 조상은";
+
+const team2 = "동문후원";
+const teamMates2 = "00 | OOO\n00 | OOO OOO OOO OOO OOO OOO OOO OOO\n00 | OOO OOO OOO OOO OOO OOO";
+
+const team3 = "행사 준비";
+const teamMates3 = "전시 전반 | OOO OOO\n번역 | OOO";
+
+const team4 = "제작";
+const teamMates4 = "웹사이트 개발 | OOO\n현장 촬영 | OOO\n도록 인쇄 | OOO\n포스터 인쇄 | OOO\n굿즈 제작 | OOO";
+
+const ThanksToComponent: React.FC<{team: string, teamMates: string}> = ({team, teamMates}) => {
+    return (
+        <div className="flex flex-row mb-[16px]">
+            <p className="text-white text-[15px] leading-[30px] tracking-[-0.165px] font-normal ml-[2px] w-[47%]">{team}</p>
+            <p className="text-white text-[15px] leading-[30px] tracking-[-0.165px] font-normal text-start whitespace-pre-line">{teamMates}</p>
+        </div>
+    );
+}
+
 export const ThanksTo = () => {
     return (
         <div className="flex flex-col mt-[96px]">
-            <p className="text-[30px] font-normal leading-[40px] tracking-[-1.5px]">도움 주신 분들</p>
-            <div className="flex flex-row items-center mt-[31.5px] mb-[16px]">
-                <p className="text-white text-[15px] leading-[30px] tracking-[-0.165px] font-normal ml-[2px] w-[47%]">
-                지도교수님
-                </p>
-                <p className="text-white text-[15px] leading-[30px] tracking-[-0.165px] font-normal text-start">
-                김수정 배민기 이장섭 이준원 안성모 유병준 장성연 조상은 
-                </p>
-            </div>
-
-            <div className="flex flex-row mb-[16px]">
-                <p className="text-white text-[15px] leading-[30px] tracking-[-0.165px] font-normal ml-[2px] w-[47%]">
-                동문후원
-                </p>
-                <p className="text-white text-[15px] leading-[30px] tracking-[-0.165px] font-normal">
-                00 | OOO<br/>00 | OOO OOO OOO OOO OOO OOO OOO OOO<br/><span className="text-transparent">00 | </span>OOO OOO OOO OOO OOO OOO 
-                </p>
-            </div>
-
-            <div className="flex flex-row mb-[16px]">
-                <p className="text-white text-[15px] leading-[30px] tracking-[-0.165px] font-normal ml-[2px] w-[47%]">
-                행사 준비
-                </p>
-                <p className="text-white text-[15px] leading-[30px] tracking-[-0.165px] font-normal">
-                전시 전반 | OOO OOO<br/>번역 | OOO
-                </p>
-            </div>
-
-            <div className="flex flex-row mb-[154px]">
-                <p className="text-white text-[15px] leading-[30px] tracking-[-0.165px] font-normal ml-[2px] w-[47%]">
-                제작
-                </p>
-                <p className="text-white text-[15px] leading-[30px] tracking-[-0.165px] font-normal">
-                웹사이트 개발 | OOO<br/>현장 촬영 | OOO<br/>도록 인쇄 | OOO<br/>포스터 인쇄 | OOO<br/>굿즈 제작 | OOO
-                </p>
-            </div>
+            <p className="text-[30px] font-normal leading-[40px] tracking-[-1.5px] mb-[31.5px]">도움 주신 분들</p>
+            <ThanksToComponent team={team1} teamMates={teamMates1} />
+            <ThanksToComponent team={team2} teamMates={teamMates2} />
+            <ThanksToComponent team={team3} teamMates={teamMates3} />
+            <ThanksToComponent team={team4} teamMates={teamMates4} />
+            <div className="mb-[138px]"></div>
         </div>
     );
 }
