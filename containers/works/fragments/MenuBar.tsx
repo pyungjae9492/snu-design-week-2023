bedffd01a5dd8ba3a4341587b7d8c6c2f3d6b6d8
eojin03 <elena1014@naver.com>
2023-11-14 19:30:58 +0900

    feat: navigation bar

--- containers/works/fragments/MenuBar.tsx
+++ containers/works/fragments/MenuBar.tsx
@@ -3,37 +3,134 @@ import React, { useState } from 'react'
 
 import ellipse from 'public/ellipse.png'
 
-export const MenuBar = () => {
-  const menuItems = [
-    'ALL',
-    'BRAND',
-    'UX/UI',
-    'MEDIA',
-    'GRAPHIC',
-    'PRODUCT INTERACTION',
-    'LIVING',
-    'MOBILITY',
-    'SPACE'
-  ]
-  const [isMenuOpen, setIsMenuOpen] = useState(true)
+import styles from './menubar.module.css'
 
-  const handleClickMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
+{
+  /*props: isMenuOpen, setIsMenuOpen, selectedMenu, setSelectedMenu*/
+}
+export default function MenuBar(props) {
+  const [selectedMenu, setSelectedMenu] = useState(props.selectedMenu)
+
+  const beforeOpenMenu = (menu: string) => (
+    <div className={`flex flex-row items-center gap-10`}>
+      <Image src={ellipse} width='17px' height='17px' layout='fixed' />
+      <div
+        onClick={() => onSelectMenu(menu)}
+        className='flex flex-wrap text-5xl  cursor-pointer border border-red-400'
+      >
+        {menu}
+      </div>
+    </div>
+  )
+
+  const onSelectMenu = (menu: string) => {
+    props.setIsMenuOpen(!props.isMenuOpen)
+    setSelectedMenu(menu)
   }
 
   return (
-    <div className='flex flex-col w-1/2 gap-[50px]'>
-      {isMenuOpen &&
-        menuItems.map((item, index) => (
-          <div
-            key={index}
-            className='flex flex-row'
-            style={{ transform: `rotate(${isMenuOpen ? index * 7 : 0}deg)` }}
-          >
-            <Image src={ellipse} width='18px' height='18px' />
-            <div className='text-white text-3xl'>{item}</div>
-          </div>
-        ))}
+    <div className='w-[500px] max-h-[300px]'>
+      {props.isMenuOpen ? (
+        <div className={styles.circle}>
+          <>
+            <div className={styles.all}>{beforeOpenMenu('ALL')}</div>
+            <div className={styles.brand}>{beforeOpenMenu('BRAND')}</div>
+            <div className={styles.uxui}>{beforeOpenMenu('UX/UI')}</div>
+            <div className={styles.media}>{beforeOpenMenu('MEDIA')}</div>
+            <div className={styles.graphic}>{beforeOpenMenu('GRAPHIC')}</div>
+            <div className={styles.product}>
+              {beforeOpenMenu('PRODUCT INTERACTION')}
+            </div>
+            <div className={styles.living}>{beforeOpenMenu('LIVING')}</div>
+            <div className={styles.mobility}>{beforeOpenMenu('MOBILITY')}</div>
+            <div className={styles.space}>{beforeOpenMenu('SPACE')}</div>
+          </>
+        </div>
+      ) : (
+        <div className={styles.circle}>
+          <>
+            <div
+              className={
+                selectedMenu === 'ALL' ? styles.allselected : styles.allreverse
+              }
+            >
+              {beforeOpenMenu('ALL')}
+            </div>
+            <div
+              className={
+                selectedMenu === 'BRAND'
+                  ? styles.brandselected
+                  : styles.brandreverse
+              }
+            >
+              {beforeOpenMenu('BRAND')}
+            </div>
+            <div
+              className={
+                selectedMenu === 'UX/UI'
+                  ? styles.uxuiselected
+                  : styles.uxuireverse
+              }
+            >
+              {beforeOpenMenu('UX/UI')}
+            </div>
+            <div
+              className={
+                selectedMenu === 'MEDIA'
+                  ? styles.mediaselected
+                  : styles.mediareverse
+              }
+            >
+              {beforeOpenMenu('MEDIA')}
+            </div>
+            <div
+              className={
+                selectedMenu === 'GRAPHIC'
+                  ? styles.graphicselected
+                  : styles.graphicreverse
+              }
+            >
+              {beforeOpenMenu('GRAPHIC')}
+            </div>
+            <div
+              className={
+                selectedMenu === 'PRODUCT INTERACTION'
+                  ? styles.productselected
+                  : styles.productreverse
+              }
+            >
+              {beforeOpenMenu('PRODUCT INTERACTION')}
+            </div>
+            <div
+              className={
+                selectedMenu === 'LIVING'
+                  ? styles.livingselected
+                  : styles.livingreverse
+              }
+            >
+              {beforeOpenMenu('LIVING')}
+            </div>
+            <div
+              className={
+                selectedMenu === 'MOBILITY'
+                  ? styles.mobilityselected
+                  : styles.mobilityreverse
+              }
+            >
+              {beforeOpenMenu('MOBILITY')}
+            </div>
+            <div
+              className={
+                selectedMenu === 'SPACE'
+                  ? styles.spaceselected
+                  : styles.spacereverse
+              }
+            >
+              {beforeOpenMenu('SPACE')}
+            </div>
+          </>
+        </div>
+      )}
     </div>
   )
 }
--- containers/works/index.tsx
+++ containers/works/index.tsx
@@ -1,28 +1,38 @@
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 
+import { is } from 'date-fns/locale'
 import ellipse from 'public/ellipse.png'
 
-import { MenuBar } from './fragments/MenuBar'
+import MenuBar from './fragments/MenuBar'
 import { MenuExplanation } from './fragments/MenuExplanation'
 import { ProjectImage } from './fragments/ProjectImage'
 
 export const WorksContainer = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
+  const [selectedMenu, setSelectedMenu] = useState('ALL')
+
   return (
     <div className='w-full h-max flex-col lg:flex-row'>
       <div className='max-h-screen flex-col mx-6 lg:flex lg:flex-row lg:justify-between'>
         {/*메뉴와 메뉴 설명*/}
         <div className='flex flex-col lg:justify-between lg:mr-12'>
-          <div className='flex flex-col items-center gap-10 lg:flex-row '>
-            <Image
+          <div>
+            {/* <Image
               src={ellipse}
-              width={30}
-              height={30}
-              className='invisible lg:visible'
+              width='17px'
+              height='17px'
+              layout='fixed'
+              className='fixed top-0 left-1 visible sm:invisible '
+            /> */}
+            {/* <div className='text-5xl'>{selectedMenu}</div> */}
+            <MenuBar
+              isMenuOpen={isMenuOpen}
+              setIsMenuOpen={setIsMenuOpen}
+              selectedMenu={selectedMenu}
+              setSelectedMenu={selectedMenu}
             />
-            <div className='text-5xl'>GRAPHIC</div>
           </div>
           <div className='lg:ml-16 lg:mb-10'>
             {!isMenuOpen && <MenuExplanation />}
