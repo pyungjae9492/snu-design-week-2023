62266f35b3c866cd3ca951cec83804f4d3212910
eojin03 <elena1014@naver.com>
2023-11-18 10:52:23 +0900

    feat: add MobileMenuBar

--- containers/works/fragments/ProjectImage.tsx
+++ containers/works/fragments/ProjectImage.tsx
@@ -5,8 +5,20 @@ import projectcover from 'public/projectcover.png'
 
 export const ProjectImage = () => {
   return (
-    <div className='w-full'>
-      <Image src={projectcover} layout='responsive' />
+    <div className='w-full relative'>
+      <Image src={projectcover} layout='responsive'/>
+      <div className='absolute left-5 bottom-4 lg:hidden'>
+        <div className='flex flex-row items-center gap-4'>
+          <div className='flex flex-col'>
+            <div className='text-base font-medium'>무슨 프로젝트</div>
+            <div className='text-base font-medium'>Design Project</div>
+          </div>
+          <div className='flex flex-col'>
+            <div className='text-xs font-medium'>디자이너 |  김이름</div>
+            <div className='text-xs font-medium'>Designer | Name</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
