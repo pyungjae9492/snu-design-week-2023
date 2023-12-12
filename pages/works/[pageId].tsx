5154a9a177fa17d5ec3eaf4ea08ad57ce4b3425c
pyungjae9492 <ppj050@snu.ac.kr>
2023-12-12 15:10:31 +0900

    Fix and rebase dodo's work

--- pages/works/[pageId].tsx
+++ pages/works/[pageId].tsx
@@ -179,64 +179,64 @@ export default function NotionDomainDynamicPage(props) {
         </div>
       </div>
 
-          <div className='w-full flex justify-center'>
-            <div className='max-w-[1600px] w-full'>
-              <div className='lg:hidden pt-10 pb-6'>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="294" height="2" viewBox="0 0 294 2" fill="none" className='mx-auto'>
-                    <path d="M0 1H294" stroke="url(#paint0_linear_780_20366)" stroke-width="2"/>
-                    <defs>
-                      <linearGradient id="paint0_linear_780_20366" x1="1.31839" y1="1.50011" x2="300.592" y2="1.49947" gradientUnits="userSpaceOnUse">
-                        <stop stop-opacity="0.546875"/>
-                        <stop offset="0.5" stop-color="white"/>
-                        <stop offset="1" stop-opacity="0"/>
-                      </linearGradient>
-                    </defs>
-                  </svg>
-              </div>
-              <div className='sm:hidden py-[100px]'>
-                  <svg xmlns="http://www.w3.org/2000/svg" height="2" viewBox="0 0 1400 2" fill="none" className='w-[90%] mx-auto'>
-                    <path d="M0 1H1400" stroke="white" stroke-width="0.853058"/>
-                  </svg>
-              </div>
-              <div className='w-[90%] lg:w-[75%] mx-auto'>
-                <StudentInfo {...pageProperties} />
-              </div>
-              <div className='sm:hidden pt-[100px]'>
-                  <svg xmlns="http://www.w3.org/2000/svg" height="2" viewBox="0 0 1400 2" fill="none" className='w-[90%] mx-auto'>
-                    <path d="M0 1H1400" stroke="white" stroke-width="0.853058"/>
-                  </svg>
-              </div>
-              <div className='flex flex-row justify-between mx-auto w-[90%] lg:w-[75%] mt-10 lg:mt-[50px] mb-[130px] lg:mb-[180px]'>
-                {prevPage && (
-                  <div 
-                    className='flex flex-col lg:flex-row-reverse lg:items-center gap-2 lg:gap-7 cursor-pointer'
-                    onClick={() => {
-                      router.push(`/works/${prevPage}`)
-                    }}
-                  >
-                    <p className='text-base lg:text-2xl'>Previous</p>
-                    <svg xmlns='http://www.w3.org/2000/svg' width='52' height='11' viewBox='0 0 52 11' fill='none'>
-                      <path d='M7.1042 0.57008C4.59995 -0.625493 1.62777 0.493843 0.465648 3.07019C-0.696471 5.64653 0.391545 8.70428 2.8958 9.89985C5.40005 11.0954 8.37223 9.97609 9.53435 7.39974C10.6965 4.8234 9.60846 1.76565 7.1042 0.57008Z' fill='white'/>
-                      <path d='M52 4.84312L4 5.8125L4.01184 3.8125L52 4.84312Z' fill='white'/>
-                    </svg>
-                  </div>
-                )}
-                {nextPage && (
-                  <div 
-                    className='flex flex-col items-end lg:flex-row lg:items-center gap-2 lg:gap-7 cursor-pointer'
-                    onClick={() => {
-                      router.push(`/works/${nextPage}`)
-                    }}
-                  >
-                    <p className='text-base lg:text-2xl'>Next</p>
-                    <svg xmlns='http://www.w3.org/2000/svg' width='52' height='11' viewBox='0 0 52 11' fill='none'>
-                      <path d='M42.513 0.57008C45.0172 -0.625493 47.9894 0.493843 49.1515 3.07019C50.3137 5.64653 49.2256 8.70428 46.7214 9.89985C44.2171 11.0954 41.245 9.97609 40.0828 7.39974C38.9207 4.8234 40.0087 1.76565 42.513 0.57008Z' fill='white'/>
-                      <path d='M-4.37892e-08 5.03062L48 6L47.9882 4L-4.37892e-08 5.03062Z' fill='white'/>
-                    </svg>
-                  </div>
-                )}
+        <div className='w-full flex justify-center'>
+          <div className='max-w-[1600px] w-full'>
+            <div className='lg:hidden pt-10 pb-6'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="294" height="2" viewBox="0 0 294 2" fill="none" className='mx-auto'>
+                  <path d="M0 1H294" stroke="url(#paint0_linear_780_20366)" stroke-width="2"/>
+                  <defs>
+                    <linearGradient id="paint0_linear_780_20366" x1="1.31839" y1="1.50011" x2="300.592" y2="1.49947" gradientUnits="userSpaceOnUse">
+                      <stop stop-opacity="0.546875"/>
+                      <stop offset="0.5" stop-color="white"/>
+                      <stop offset="1" stop-opacity="0"/>
+                    </linearGradient>
+                  </defs>
+                </svg>
             </div>
+            <div className='sm:hidden py-[100px]'>
+                <svg xmlns="http://www.w3.org/2000/svg" height="2" viewBox="0 0 1400 2" fill="none" className='w-[90%] mx-auto'>
+                  <path d="M0 1H1400" stroke="white" stroke-width="0.853058"/>
+                </svg>
+            </div>
+            <div className='w-[90%] lg:w-[75%] mx-auto'>
+              <StudentInfo {...pageProperties} />
+            </div>
+            <div className='sm:hidden pt-[100px]'>
+                <svg xmlns="http://www.w3.org/2000/svg" height="2" viewBox="0 0 1400 2" fill="none" className='w-[90%] mx-auto'>
+                  <path d="M0 1H1400" stroke="white" stroke-width="0.853058"/>
+                </svg>
+            </div>
+            <div className='flex flex-row justify-between mx-auto w-[90%] lg:w-[75%] mt-10 lg:mt-[50px] mb-[130px] lg:mb-[180px]'>
+              {prevPage && (
+                <div 
+                  className='flex flex-col lg:flex-row-reverse lg:items-center gap-2 lg:gap-7 cursor-pointer'
+                  onClick={() => {
+                    router.push(`/works/${prevPage}`)
+                  }}
+                >
+                  <p className='text-base lg:text-2xl'>Previous</p>
+                  <svg xmlns='http://www.w3.org/2000/svg' width='52' height='11' viewBox='0 0 52 11' fill='none'>
+                    <path d='M7.1042 0.57008C4.59995 -0.625493 1.62777 0.493843 0.465648 3.07019C-0.696471 5.64653 0.391545 8.70428 2.8958 9.89985C5.40005 11.0954 8.37223 9.97609 9.53435 7.39974C10.6965 4.8234 9.60846 1.76565 7.1042 0.57008Z' fill='white'/>
+                    <path d='M52 4.84312L4 5.8125L4.01184 3.8125L52 4.84312Z' fill='white'/>
+                  </svg>
+                </div>
+              )}
+              {nextPage && (
+                <div 
+                  className='flex flex-col items-end lg:flex-row lg:items-center gap-2 lg:gap-7 cursor-pointer'
+                  onClick={() => {
+                    router.push(`/works/${nextPage}`)
+                  }}
+                >
+                  <p className='text-base lg:text-2xl'>Next</p>
+                  <svg xmlns='http://www.w3.org/2000/svg' width='52' height='11' viewBox='0 0 52 11' fill='none'>
+                    <path d='M42.513 0.57008C45.0172 -0.625493 47.9894 0.493843 49.1515 3.07019C50.3137 5.64653 49.2256 8.70428 46.7214 9.89985C44.2171 11.0954 41.245 9.97609 40.0828 7.39974C38.9207 4.8234 40.0087 1.76565 42.513 0.57008Z' fill='white'/>
+                    <path d='M-4.37892e-08 5.03062L48 6L47.9882 4L-4.37892e-08 5.03062Z' fill='white'/>
+                  </svg>
+                </div>
+              )}
           </div>
+        </div>
       </div>
     </Layout>
   )
