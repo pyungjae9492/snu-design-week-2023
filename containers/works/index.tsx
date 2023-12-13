f2e3e2c4428b070bbd7888be58758c4bf796f44c
Dohyeon Park <dodo41142727@gmail.com>
2023-12-13 17:19:40 +0900

    Dodo edit (#35)
    * Save menu as url query
    
    * Fix selectedMenu as query param
    
    * Fix scroll
    
    * Change isMenuOpen default
    
    * Make scroll smooth
    
    ---------
    
    Co-authored-by: Dohyeon Park <dodo@acon3d.com>

--- containers/works/index.tsx
+++ containers/works/index.tsx
@@ -1,20 +1,53 @@
-import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
 
 import MenuBar from './fragments/MenuBar'
-import { MobileMenuBar } from './fragments/MobileMenuBar'
 import { MenuExplanation } from './fragments/MenuExplanation'
+import { MobileMenuBar } from './fragments/MobileMenuBar'
 import { ProjectGallery } from './fragments/ProjectGallery'
 
 export const WorksContainer = (props) => {
+  const router = useRouter()
 
-  const [isMenuOpen, setIsMenuOpen] = useState(true)
+  const { menu } = router.query
+  const [isMenuOpen, setIsMenuOpen] = useState(!menu)
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
-  const [selectedMenu, setSelectedMenu] = useState('ALL')
+  const scrollRef = React.useRef(null)
 
+  const selectedMenu = menu ? (menu as string) : 'ALL'
   const notionProps = props[selectedMenu]
 
+  function setSelectedMenu(newMenu: string) {
+    if (newMenu !== 'ALL') {
+      // set selectedMenu to url query
+      router.push(
+        {
+          pathname: '/works',
+          query: { menu: newMenu }
+        },
+        undefined,
+        { shallow: true }
+      )
+    }
+    if (newMenu === 'ALL') {
+      router.push(
+        {
+          pathname: '/works'
+        },
+        undefined,
+        { shallow: true }
+      )
+    }
+    // scroll to top
+    scrollRef.current.scrollTo(0, 0)
+  }
+
   return (
-    <div className={`w-full mx-auto lg:w-full lg:h-[calc(100vh-150px)] flex-col lg:flex lg:flex-row px-7 lg:px-10 sm:pb-20 overflow-y-hidden ${isMobileMenuOpen && "sm:h-[calc(100vh-88px)]"}`}>
+    <div
+      className={`w-full mx-auto lg:w-full lg:h-[calc(100vh-150px)] flex-col lg:flex lg:flex-row px-7 lg:px-10 sm:pb-20 overflow-y-hidden ${
+        isMobileMenuOpen && 'sm:h-[calc(100vh-88px)]'
+      }`}
+    >
       <div className='lg:max-w-[50%] lg:h-full lg:mb-[72px] lg:flex-col lg:relative'>
         {/*sm메뉴*/}
         <div className='lg:hidden'>
@@ -26,9 +59,23 @@ export const WorksContainer = (props) => {
           />
         </div>
         {/*lg메뉴*/}
-        <div className={`sm:hidden flex flex-row w-[550px] h-[90px] items-center`}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 15 15" fill="none">
-            <circle cx="7.4084" cy="7.35111" r="6.92308" transform="rotate(50 7.4084 7.35111)" fill="white"/>
+        <div
+          className={`sm:hidden flex flex-row w-[550px] h-[90px] items-center`}
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='17'
+            height='17'
+            viewBox='0 0 15 15'
+            fill='none'
+          >
+            <circle
+              cx='7.4084'
+              cy='7.35111'
+              r='6.92308'
+              transform='rotate(50 7.4084 7.35111)'
+              fill='white'
+            />
           </svg>
         </div>
         <div className='sm:hidden'>
@@ -41,11 +88,20 @@ export const WorksContainer = (props) => {
         </div>
         {/*메뉴설명*/}
         <div className='lg:pl-8 lg:pr-[30px] lg:absolute lg:left-8 lg:right-[52px] lg:bottom-0'>
-          <MenuExplanation isMenuOpen={isMenuOpen} selectedMenu={selectedMenu}/>
+          <MenuExplanation
+            isMenuOpen={isMenuOpen}
+            selectedMenu={selectedMenu}
+          />
         </div>
       </div>
 
-      <div className='max-w-[90vw] lg:w-full lg:h-full lg:overflow-y-auto scrollbar-hide'>
+      <div
+        className='max-w-[90vw] lg:w-full lg:h-full lg:overflow-y-auto scrollbar-hide'
+        style={{
+          scrollBehavior: 'smooth'
+        }}
+        ref={scrollRef}
+      >
         <ProjectGallery {...notionProps} />
       </div>
     </div>
