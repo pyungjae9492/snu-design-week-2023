359cc839d50869f74963853511b52c80cd30b33e
Dohyeon Park <dodo@acon3d.com>
2023-12-13 15:41:59 +0900

    Save menu as url query

--- containers/works/index.tsx
+++ containers/works/index.tsx
@@ -1,20 +1,58 @@
-import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
 
 import MenuBar from './fragments/MenuBar'
-import { MobileMenuBar } from './fragments/MobileMenuBar'
 import { MenuExplanation } from './fragments/MenuExplanation'
+import { MobileMenuBar } from './fragments/MobileMenuBar'
 import { ProjectGallery } from './fragments/ProjectGallery'
 
 export const WorksContainer = (props) => {
-
   const [isMenuOpen, setIsMenuOpen] = useState(true)
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
-  const [selectedMenu, setSelectedMenu] = useState('ALL')
+  const [selectedMenu, setSelectedMenuState] = useState('ALL')
 
   const notionProps = props[selectedMenu]
 
+  const router = useRouter()
+
+  const { menu } = router.query
+
+  function setSelectedMenu(newMenu: string) {
+    if (newMenu !== 'ALL') {
+      // set selectedMenu to url query
+      router.push(
+        {
+          pathname: '/works',
+          query: { menu: newMenu }
+        },
+        undefined,
+        { shallow: true }
+      )
+    }
+    if (newMenu === 'ALL') {
+      setSelectedMenuState(newMenu)
+      router.push(
+        {
+          pathname: '/works'
+        },
+        undefined,
+        { shallow: true }
+      )
+    }
+  }
+
+  useEffect(() => {
+    if (menu) {
+      setSelectedMenuState(menu as string)
+    }
+  }, [menu])
+
   return (
-    <div className={`w-full mx-auto lg:w-full lg:h-[calc(100vh-150px)] flex-col lg:flex lg:flex-row px-7 lg:px-10 sm:pb-20 overflow-y-hidden ${isMobileMenuOpen && "sm:h-[calc(100vh-88px)]"}`}>
+    <div
+      className={`w-full mx-auto lg:w-full lg:h-[calc(100vh-150px)] flex-col lg:flex lg:flex-row px-7 lg:px-10 sm:pb-20 overflow-y-hidden ${
+        isMobileMenuOpen && 'sm:h-[calc(100vh-88px)]'
+      }`}
+    >
       <div className='lg:max-w-[50%] lg:h-full lg:mb-[72px] lg:flex-col lg:relative'>
         {/*sm메뉴*/}
         <div className='lg:hidden'>
@@ -26,9 +64,23 @@ export const WorksContainer = (props) => {
           />
         </div>
         {/*lg메뉴*/}
-        <div className={`sm:hidden flex flex-row w-[550px] h-[90px] items-center`}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 15 15" fill="none">
-            <circle cx="7.4084" cy="7.35111" r="6.92308" transform="rotate(50 7.4084 7.35111)" fill="white"/>
+        <div
+          className={`sm:hidden flex flex-row w-[550px] h-[90px] items-center`}
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='17'
+            height='17'
+            viewBox='0 0 15 15'
+            fill='none'
+          >
+            <circle
+              cx='7.4084'
+              cy='7.35111'
+              r='6.92308'
+              transform='rotate(50 7.4084 7.35111)'
+              fill='white'
+            />
           </svg>
         </div>
         <div className='sm:hidden'>
@@ -41,7 +93,10 @@ export const WorksContainer = (props) => {
         </div>
         {/*메뉴설명*/}
         <div className='lg:pl-8 lg:pr-[30px] lg:absolute lg:left-8 lg:right-[52px] lg:bottom-0'>
-          <MenuExplanation isMenuOpen={isMenuOpen} selectedMenu={selectedMenu}/>
+          <MenuExplanation
+            isMenuOpen={isMenuOpen}
+            selectedMenu={selectedMenu}
+          />
         </div>
       </div>
 
